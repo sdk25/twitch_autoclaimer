48c386f7d87fd4bae24d12295bb33fb199ee83c2
Dmitry Saltykov <25DimoN25@spark-mail.ru>
2021-09-14 04:07:47 +0300

    another try; should be reworked

--- background.js
+++ background.js
@@ -1,21 +1,56 @@
-function initState() {
-  chrome.action.setBadgeBackgroundColor({color: '#4688F1'})
-  chrome.storage.local.set({'badgeCount': 0})
-}
-
-function increaseBadge(tab) {
-  chrome.storage.local.get('badgeCount', items => {
-    const newValue = items.badgeCount + 1
-    increaseBadgeHandler(newValue, tab.id)
-    chrome.storage.local.set({'badgeCount': newValue})
+function dispatchObserver(tabId) { return () => {
+  const observer = new MutationObserver((mutations, observer) => {
+    for (const mutation of mutations) {
+      for (const addedNode of mutation.addedNodes) {
+        if (typeof addedNode.querySelector === "function") {
+          const button = addedNode.querySelector("button[aria-label='Claim Bonus']")
+          if (button) {
+            button.click()
+            chrome.storage.local.get("badgeCount_" + tabId, items => {
+              const newBadgeCount = (items.badgeCount || 0) + 1
+              chrome.storage.local.set({["badgeCount_" + tabId]: newBadgeCount}, () => {
+                const newBadgeCountString = newBadgeCount.toString()
+                chrome.action.setBadgeText({tabId: tabId, text: newBadgeCountString})
+                chrome.action.setTitle({tabId: tabId, title: "Claimed " + newBadgeCountString + " time(s)"})
+              })
+            })
+          }
+        }
+      }
+    }
   })
+
+  const container = document.querySelector(".community-points-summary")
+  observer.observe(container, {childList: true, subtree: true})
+}}
+
+function clickEventHandler(tab) {
+  chrome.action.setBadgeBackgroundColor({tabId: tab.id, color: "#4688F1"})
+  chrome.action.setBadgeText({tabId: tab.id, text: "0"})
+  chrome.action.setTitle({tabId: tab.id, title: "Autoclaimer is working.."})
+
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    func: dispatchObserver(tab.id)
+  })
+
+  chrome.action.onClicked.removeListener(clickEventHandler)
 }
 
-function increaseBadgeHandler(newValue, tabId) {
-  const newValueString = newValue.toString()
-  chrome.action.setBadgeText({tabId: tabId, text: newValueString})
-  chrome.action.setTitle({tabId: tabId, title: 'Claimed ' + newValueString +  ' time(s)'})
-}
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.url) {
+    if (/twitch.tv\/.+/i.test(changeInfo.url)) {
+      chrome.action.setBadgeBackgroundColor({tabId: tabId, color: "#09AB3F"})
+      chrome.action.setBadgeText({tabId: tabId, text: "▶"})
+      chrome.action.setTitle({tabId: tabId, title: "Click to inject an autoclaimer"})
 
-chrome.runtime.onInstalled.addListener(initState)
-chrome.action.onClicked.addListener(increaseBadge)
+      chrome.action.onClicked.addListener(clickEventHandler)
+    } else {
+      chrome.action.setBadgeBackgroundColor({tabId: tabId, color: "#940000"})
+      chrome.action.setBadgeText({tabId: tabId, text: "🞫"})
+      chrome.action.setTitle({tabId: tabId, title: "Go to any stream to enable"})
+
+      chrome.action.onClicked.removeListener(clickEventHandler)
+    }
+  }
+})
